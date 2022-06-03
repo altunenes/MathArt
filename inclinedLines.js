9de8e05826dc2f26ed87d84ffd0be9efbc597762
Enes <54986652+altunenes@users.noreply.github.com>
2022-06-03 22:13:15 +0300

    Update inclinedLines.js

--- inclinedLines.js
+++ inclinedLines.js
@@ -1,3 +1,13 @@
+var lines = [];
+var speed = 1;
+function moveLines() {
+  for (var i = 0; i < lines.length; i++) {
+    lines[i].style.left = parseInt(lines[i].style.left) + speed + 'px';
+    if (parseInt(lines[i].style.left) > window.innerWidth) {
+      lines[i].style.left = '0px';
+    }
+  }
+}
 for (var i = 0; i < 100; i++) {
   var line = document.createElement('div');
   line.style.position = 'absolute';
@@ -7,4 +17,6 @@ for (var i = 0; i < 100; i++) {
   line.style.backgroundColor = 'black';
   line.style.transform = 'rotate(' + i * 8 + 'deg)';
   document.body.appendChild(line);
+  lines.push(line);
 }
+setInterval(moveLines, 10);
