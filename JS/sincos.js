32845422b3a630989c1137eb892ee62a80dbbc9f
Enes <54986652+altunenes@users.noreply.github.com>
2022-10-18 03:31:57 +0300

    Update sincos.js

--- JS/sincos.js
+++ JS/sincos.js
@@ -107,7 +107,7 @@ document.addEventListener("DOMContentLoaded", function() {
     github.style.color = '#fff';
     document.body.appendChild(github);
     var twitter = document.createElement('a');
-    twitter.href = 'https://twitter.com/altunenes';
+    twitter.href = 'https://twitter.com/emportent';
     twitter.innerHTML = 'Twitter';
     twitter.style.position = 'fixed';
     twitter.style.bottom = '0';
